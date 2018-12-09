e4afc039f9b2c6a2b0c88c1ced7f89eb9497167f
Calvin Aw <caw@stuy.edu>
2018-12-09 00:44:58 -0500

    almost done

--- mesh.c
+++ mesh.c
@@ -4,30 +4,41 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <errno.h>
 
 int main(){
-  key_t key;
   int shmid;
   char *data;
 
   // "Well, since the type key_t is actually just a long, you can use any number you want."
-  key = 21335;
-  shmid = shmget(key, 1024, 0644 | IPC_CREAT);
+  //Creates memory
+  shmid = shmget(21135, 1024, 0644 | IPC_CREAT);
   printf("shmid: %d\n", shmid);
   data = shmat(shmid, (void *)0, 0);
   printf("data: %s\n", data);
+  if (data == (char *)-1){
+    printf("%s\n", strerror(errno));
+    exit(1);
+  }
 
 
-  printf("You want to change data: enter a string");
-  gets(data);
+  //Add/edit memory
+  printf("Do you want to change data? ");
+  printf("Enter a string: ");
+
+  fgets(data,200,stdin);
   printf("new data: %s\n", data);
 
 
-  //char * x;
-  // scanf("%s", &x);
+  while(1){
+    //clears memory
+    if (shmdt(data) == -1){
+      printf("%s\n", strerror(errno));
+      exit(1);
+    }
 
-  shmdt(data);
-  printf("cleared data: %s\n", data);
-
-  shmctl(shmid, IPC_RMID, NULL);
+    //delete memory
+    shmctl(shmid, IPC_RMID, NULL);
+    printf("memory deleted\n");
+  }
 }
